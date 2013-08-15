a95b7ff672016efc5c0ccccf33415d5778401d4c
Norio Kimura <kimura@monad.tv>
2013-08-15 15:04:14 +0900

    *

--- nx-home-rc.rb
+++ nx-home-rc.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class NxHomeRc < Formula
-  url 'http://blaise.pinklattice.com/homebrew/nx-home-rc_0.1.22.tar.gz'
+  url 'http://blaise.pinklattice.com/homebrew/nx-home-rc_0.1.23.tar.gz'
   homepage 'http://blaise.pinklattice.com/homebrew/nx-home-rc/'
-  sha1 '97ae12b83770f64f87fbe2c84a19db6961ea9fbd'
+  sha1 '99f22436b67f5797b23d396011a00999'
 
   def install
     system "make PREFIX=#{prefix}"
